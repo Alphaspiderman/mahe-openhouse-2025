ae90a34e98f51d7fc11e4e8bc888fcc9925d8dba
Abhigyan Trips <contact@abhigyantrips.dev>
2025-04-08 11:59:18 +0530

    fix(ui): styling for install button

--- src/components/install-button.tsx
+++ src/components/install-button.tsx
@@ -37,7 +37,7 @@ const InstallButton = () => {
   return (
     <>
       {isInstallable && (
-        <Button onClick={handleInstallClick} className="install-button">
+        <Button onClick={handleInstallClick} className="uppercase">
           Install App
         </Button>
       )}
