50cff176198aa23d9f95763434aae64f533ddde4
Abhigyan Trips <contact@abhigyantrips.dev>
2025-04-08 11:53:31 +0530

    feat(ui): add install button for PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Work_Sans } from 'next/font/google';
 import Image from 'next/image';
 
+import InstallButton from '@/components/install-button';
+
 import bannerInstitute from '@/assets/banner-institute.webp';
 import bannerLogo from '@/assets/banner-logo.webp';
 
@@ -36,19 +38,23 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`${fontSans.className} antialiased`}>
-        <header className="m-4 items-start space-y-2">
-          <Image
-            src={bannerInstitute}
-            alt="Institute Banner"
-            className="w-24 invert"
-            priority
-          />
-          <Image
-            src={bannerLogo}
-            alt="Logo Banner"
-            className="w-36 invert"
-            priority
-          />
+        <header className="m-4 flex items-center justify-between">
+          <div className="space-y-2">
+            <Image
+              src={bannerInstitute}
+              alt="Institute Banner"
+              className="w-24 invert"
+              priority
+            />
+            <Image
+              src={bannerLogo}
+              alt="Logo Banner"
+              className="w-36 invert"
+              priority
+            />
+          </div>
+
+          <InstallButton />
         </header>
         <main className="mx-4 my-8 lg:mx-10">{children}</main>
         <footer className="m-4 items-start font-bold uppercase">
